ffaca35e4b3877d76fc0eee2e1a7d2262f33d94d
blueberryGhoul <skorobogatowork@gmail.com>
2023-06-13 19:55:24 +0300

    Create file structure.

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "pointers.h"
+#include <iostream>
+
+int main()
+{
+	testStrStrF();
+	system("pause");
+	return 0;
+}
---
+++ pointers.h
@@ -0,0 +1,14 @@
+#ifndef POINTERS_H
+#define POINTERS_H
+
+struct testStrStr;
+
+unsigned strLenF(const char *str);
+
+void strCatF(char* to, const char* from);
+
+int strStrF(const char *text, const char *pattern);
+
+void testStrStrF();
+
+#endif
